fb98e395df9dc9d62a86c5b5d2e90a6d6d0d5e0f
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-06 01:36:35 -0700

    primative types

---
+++ Type/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        byte age = 30; // we can store values up to 127 
+        int viewsCount = 123_456_789; // we can use an undersocre (_) as commas
+        long moreViews = 3_123_456_789L; // we hava to add an "L" as a suffix for this number
+        float price = 10.99F; //java sees the number "10.99" as a double because of the decimal so we have to add "F" as a suffix to represent float
+        char letter = 'A'; // single characters with single quotes, multiple characters with double quotes
+        boolean isEligible = false;
+    }
+}
