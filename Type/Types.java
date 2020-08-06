78c13a49273447fa668ef978862dfd3a9c81af65
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-06 02:04:08 -0700

    Refrence Type, Gives you current time

--- Type/Types.java
+++ Type/Types.java
@@ -2,7 +2,8 @@ import java.util.Date;
 
 public class Types {
     public static void main(String[] args){
-       byte age = 30; 
+        // Refrence type
+        byte age = 30; 
         Date now = new Date(); // variable is an instance of the class Date
        // now.getTime(); //dot gives you members in refrence type
         System.out.println(now); //type sout tab,  gives us System.out.println
