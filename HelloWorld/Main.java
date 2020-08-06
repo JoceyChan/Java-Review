9febfaedbe2cad481ebb09f8a9e7bc9e129ea810
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-06 01:16:17 -0700

    Initializing variable and printing variable

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void  main(String[] args){
-        System.out.println("Hello World");
+        int myAge = 20;
+
+        System.out.print("Hello World, I am Jocelyn Chan, I am ");
+        System.out.print(myAge);
+        System.out.println(" years old.");
+
     }
 }
