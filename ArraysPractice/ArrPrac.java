8aca1d53890a031ef1cc2b5017428dcc70de27b3
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-07 01:25:57 -0700

    Arrays to string

---
+++ ArraysPractice/ArrPrac.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class ArrPrac {
+
+    public static void main(String[] args) {
+        int[] numbers = new int [5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        // Arrays.toString(numbers); //string representation of the array
+        System.out.println(Arrays.toString(numbers));
+    }
+}
