3ea2dcf39cada2c2a93d2d07e586ed6fa35869fb
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-08 20:09:35 -0700

    Array numbers

--- ArraysPractice/ArrPrac.java
+++ ArraysPractice/ArrPrac.java
@@ -1,11 +1,12 @@
+package ArraysPractice;
+
 import java.util.Arrays;
 
 public class ArrPrac {
 
     public static void main(String[] args) {
-        int[] numbers = new int [5];
-        numbers[0] = 1;
-        numbers[1] = 2;
+        int[] numbers = {2, 3, 5, 1, 4};
+        System.out.println(numbers.length);
         // Arrays.toString(numbers); //string representation of the array
         System.out.println(Arrays.toString(numbers));
     }
