fe466165bae4779058ebe442d2da58397ad06c46
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 01:58:00 -0700

    initialized variables

--- MortgageCalcProj/MortgagaCalc.java
+++ MortgageCalcProj/MortgagaCalc.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 public class MortgagaCalc {
     public static void main(String[] args){
-
+        int p, m, r, n;
         Scanner scan = new Scanner(System.in);
-
     }
 }
