d2a0776439c8699cebb5656f3e984f245d90296f
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-09 19:44:13 -0700

    final float

---
+++ ArraysPractice/Constants/Const.java
@@ -0,0 +1,9 @@
+package ArraysPractice.Constants;
+
+public class Const {
+    public static void main(String[] args){
+       final float PI = 3.14F; //once we initialize this as final it can no longer be changed later on
+         
+    }
+    
+}
