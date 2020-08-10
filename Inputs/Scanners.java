c6805f6fcd235d6a0c5e309443d307d9190ae0b3
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 01:28:34 -0700

    Asking user for age

--- Inputs/Scanners.java
+++ Inputs/Scanners.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Scanners {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Age: ");
         byte age = scanner. nextByte();
         System.out.println("You are " + age );
     }
