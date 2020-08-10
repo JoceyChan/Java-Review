a3afe83a3993b71b8f8c9a76ac073ebcd83ae13c
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-09 23:11:48 -0700

    Implicit Casting

---
+++ Casting/Cast.java
@@ -0,0 +1,9 @@
+public class Cast {
+    public static void main(String[] args){
+        //implicit casting 
+        short x = 1; // short has two bytes 
+        int y = x + 2; //int has 4 bytes
+        System.out.println(y);
+    }
+    
+}
