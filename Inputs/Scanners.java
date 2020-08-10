aac9274b933f956fd6324ed30a5be571ae24dcc2
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 01:36:42 -0700

    Input name

--- Inputs/Scanners.java
+++ Inputs/Scanners.java
@@ -5,5 +5,11 @@ public class Scanners {
         System.out.print("Age: ");
         byte age = scanner. nextByte();
         System.out.println("You are " + age );
+
+        System.out.print("Name: ");
+        String name = scanner.nextLine(); //for string you don't need .nextString(); it is just .next();
+        System.out.println("You are " + name); //Only prints out the first name and not the last name, when inputed both names
+        // the printed words are called tokens so in order to print out both tokens we need to call the next method twice NOT IDEAL 
+        // therefore use .nectLine()
     }
 }
