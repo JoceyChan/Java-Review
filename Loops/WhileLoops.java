d18f1cfad4ba5d3b1a45d839723fb80b49852686
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 16:36:35 -0700

    Another way

--- Loops/WhileLoops.java
+++ Loops/WhileLoops.java
@@ -13,7 +13,19 @@ public class WhileLoops {
             while (!input.equals("quit")){
                 System.out.println("Input: ");
                 input = scanner.next().toLowerCase();
-                System.out.println(input);
+                if(input.equals("pass"))
+                if(!input.equals("quit"))
+                    System.out.println(input);
             }
+
+            // while (true){
+            //     System.out.println("Input: ");
+            //     input = scanner.next().toLowerCase();
+            //     if(input.equals("pass"))
+            //         continue; //moves control to the beginning of the loop
+            //     if(!input.equals("quit"))
+            //         break;//break statment terminates a loop
+            //     System.out.println(input);
+            // }
     }
 }
