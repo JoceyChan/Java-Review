09f97ae5dd451149c6a48afcadff77352db089a1
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 01:59:21 -0700

    backspaced

--- MortgageCalcProj/MortgagaCalc.java
+++ MortgageCalcProj/MortgagaCalc.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 public class MortgagaCalc {
     public static void main(String[] args){
-        int p, m, r, n;
         Scanner scan = new Scanner(System.in);
     }
 }
