c1d508bfd7f63e400a7a1b7c0a839613dea9e025
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 13:16:24 -0700

    while loop

--- Loops/WhileLoops.java
+++ Loops/WhileLoops.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class WhileLoops {
     public static void main(String[] args) {
         int i = 0;
@@ -5,5 +7,13 @@ public class WhileLoops {
            System.out.println("Hello World" + i);
            i--; 
         }
+
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+            while (!input.equals("quit")){
+                System.out.println("Input: ");
+                input = scanner.next().toLowerCase();
+                System.out.println(input);
+            }
     }
 }
