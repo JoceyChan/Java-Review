ef3977af06c9df2e02da3c9f74fd40b0444837b4
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 12:39:01 -0700

    Notes of how for loop works

--- ForLoops/Main.java
+++ ForLoops/Main.java
@@ -1,6 +1,9 @@
 public class Main {
    public static void main(String[] args) {
        for(int i = 0; i < 5; i++){
+    //initalizin i to 0; 
+    //check if i is less than 5, moves to the next line and gets execute; 
+    //then i gets incremented by 1 and repeat the processes till i is greater or = to 5 and stops before printing
         System.out.println("Hello World");
        }
    } 
