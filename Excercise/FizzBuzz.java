05d3116b2399e82cf4c701f5e76f2273daf32837
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 12:16:48 -0700

    divisible by 5

--- Excercise/FizzBuzz.java
+++ Excercise/FizzBuzz.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class FizzBuzz {
     public static void main(String[] args) {
         //number if number is divisbale by 5 print fizz
@@ -5,5 +6,15 @@ public class FizzBuzz {
     //divisible by 3 pribt buzz
     // if divisible by both 5 and 3 print fizz buzz
     // if not divisble the print number
+    
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Number: ");
+
+    int n = scan.nextInt();
+
+    if (n % 5 == 0){
+        System.out.println("Fizz");
+    }
+        
     }
 }
