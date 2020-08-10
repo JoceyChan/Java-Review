63ec5f7d8fcd97f3f7e32f32bf585bcacbdc3e60
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-09 23:32:35 -0700

    Implicit casting

--- Casting/Cast.java
+++ Casting/Cast.java
@@ -1,6 +1,7 @@
 public class Cast {
     public static void main(String[] args){
-        //implicit casting 
+        //implicit casting, automatic casting or automatic conversion
+        // byte > short > int > long
         short x = 1; // short has two bytes 
         int y = x + 2; //int has 4 bytes
         System.out.println(y);
