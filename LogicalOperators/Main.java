67194d446c5aba7600800cfda7667e6f019a4b2f
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 10:49:44 -0700

    && operator

--- LogicalOperators/Main.java
+++ LogicalOperators/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args){
-        
+        int temperature = 22;
+        boolean isWarm = temperature > 20 && temperature < 30;  // boolean is true or false, "&&"" is "and" 
+        System.out.println(isWarm);
+        // if one of the conditions is false then the result would be false as both conditions are not true
     }
 }
