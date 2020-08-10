090cea1fd370fa96008053bbabce654489910f80
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 10:27:32 -0700

    testing

--- MortgageCalcProj/MortgagaCalc.java
+++ MortgageCalcProj/MortgagaCalc.java
@@ -12,15 +12,15 @@ public class MortgagaCalc {
 
         System.out.print("Annual Interest Rate: ");
         float r = scan.nextFloat();
+        float monthlyInterest = r / percent / months_in_year;
 
         System.out.print("Period (Years): ");
         byte years = scan.nextByte();
         int n = years * months_in_year;
 
-        float monthlyInterest = r / percent / months_in_year;
-        double a = 1 + year;
-        double period = n * 12;
-        double top = year * Math.pow(a, period);
+        double a = 1 + monthlyInterest;
+        double period = n;
+        double top = monthlyInterest * Math.pow(a, period);
         double bottom = Math.pow(a, period) - 1;
         double together = p * (top/bottom);
 
