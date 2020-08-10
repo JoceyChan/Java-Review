691aab4f82b8a45926107b68d95e8f57937dcf0b
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 16:43:51 -0700

    For each loop

--- Loops/ForEachLoop.java
+++ Loops/ForEachLoop.java
@@ -1,5 +1,12 @@
 public class ForEachLoop {
     public static void main(String[] args) {
         String[] fruits = {"Apple", "Mango", "Orange"};
+
+        for(int i = 0; i < fruits.length; i++){
+            System.out.println(fruits[i]); //returns the item at the given index
+        }
+        for(String fruit : fruits){ // we do not have acces to the index
+            System.out.println(fruit); 
+        }
     }
 }
