cb67ea5e0e23941a5cae0d6f53c1b1f0436c2361
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 10:54:03 -0700

    "||", operator

--- LogicalOperators/Main.java
+++ LogicalOperators/Main.java
@@ -4,5 +4,11 @@ public class Main {
         boolean isWarm = temperature > 20 && temperature < 30;  // boolean is true or false, "&&"" is "and" 
         System.out.println(isWarm);
         // if one of the conditions is false then the result would be false as both conditions are not true
+
+        boolean highIncome = true;
+        boolean goodCredit = true;
+        boolean criminalRecord = false;
+        boolean elgible = (highIncome || goodCredit) && !criminalRecord; // "||" is the "or" operator
+        System.out.println(elgible);
     }
 }
