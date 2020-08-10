61c956ef43178076d22b439eb7547df153e6d5c2
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 01:27:09 -0700

    creating scanner and printing the age

--- Inputs/Scanners.java
+++ Inputs/Scanners.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 public class Scanners {
     public static void main(String[] args){
-        
+        Scanner scanner = new Scanner(System.in);
+        byte age = scanner. nextByte();
+        System.out.println("You are " + age );
     }
 }
