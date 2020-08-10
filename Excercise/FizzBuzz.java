eccc60e5746bf230213cf4a5b81efff5c5f53bda
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 12:12:17 -0700

    pseudo code

--- Excercise/FizzBuzz.java
+++ Excercise/FizzBuzz.java
@@ -1,5 +1,9 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-        
+        //number if number is divisbale by 5 print fizz
+        // if 10 print fizz 
+    //divisible by 3 pribt buzz
+    // if divisible by both 5 and 3 print fizz buzz
+    // if not divisble the print number
     }
 }
