b56df328f5f62a0e948f6418226b442fba86c392
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 12:52:25 -0700

    While loop

--- Loops/WhileLoops.java
+++ Loops/WhileLoops.java
@@ -1,5 +1,9 @@
 public class WhileLoops {
     public static void main(String[] args) {
-        
+        int i = 0;
+        while (i > 0){ //use when we do not know how many times we want to print
+           System.out.println("Hello World" + i);
+           i--; 
+        }
     }
 }
