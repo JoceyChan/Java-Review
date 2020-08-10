c438c24a9f5207a45acb8f47e13f5626f3b4e688
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-09 19:02:21 -0700

    renamded

--- ArraysPractice/Main.java
+++ ArraysPractice/Main.java
@@ -2,7 +2,7 @@ package ArraysPractice;
 
 import java.util.Arrays;
 
-public class ArrPrac {
+public class Main {
 
     public static void main(String[] args) {
         int[] numbers = {2, 3, 5, 1, 4};
