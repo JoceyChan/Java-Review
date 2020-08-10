dab204ea2d2cb6f4c424a7204b3a47237048fdb0
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-09 19:00:25 -0700

    Array.sort

--- ArraysPractice/ArrPrac.java
+++ ArraysPractice/ArrPrac.java
@@ -7,6 +7,7 @@ public class ArrPrac {
     public static void main(String[] args) {
         int[] numbers = {2, 3, 5, 1, 4};
         System.out.println(numbers.length);
+        Arrays.sort(numbers);
         // Arrays.toString(numbers); //string representation of the array
         System.out.println(Arrays.toString(numbers));
     }
