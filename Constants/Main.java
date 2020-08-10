a37550defe63dfb0d23f2de77ac8978a3d104693
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-09 19:50:18 -0700

    Main

--- Constants/Main.java
+++ Constants/Main.java
@@ -1,4 +1,4 @@
-public class Const {
+public class Main {
     public static void main(String[] args){
        final float PI = 3.14F; //once we initialize this as final it can no longer be changed later on
          
