159abaf531141355d99cbd5630071621c66ccc07
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 11:57:36 -0700

    Initializing boolean, using ternary operator

--- IfStatments/IfSimplifying.java
+++ IfStatments/IfSimplifying.java
@@ -1,7 +1,8 @@
 public class IfSimplifying {
     public static void main(String[] args){
         int income = 120_000;
-        boolean hasHighIncome = (income > 100_000);
-  
-    }
+        boolean hasHighIncome = (income > 100_000); //in order to not use if else statment we just initialize where we declared
+
+        String className = income > 100_000 ? "First" : "Economy"; // use ternary operator which is the "?" and the ":"
+            }
 }
