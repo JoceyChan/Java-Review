341bb2a23f1d147ee098f27b76d1c9397d0d254f
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 01:11:56 -0700

    Displaying currency comment

--- FormatNums/FormatNum.java
+++ FormatNums/FormatNum.java
@@ -1,6 +1,7 @@
 public class FormatNum {
     public static void main(String[] args){
-        
+        // if you want to display this to the user add a "$" and every three digits add a commas as the following:
+        // $1,234,567 
     }
     
 }
