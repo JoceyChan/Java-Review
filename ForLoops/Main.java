0f06e5e60b7ea19ba2eec5d9f25bc011a16672c3
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 12:47:36 -0700

    decrementing

--- ForLoops/Main.java
+++ ForLoops/Main.java
@@ -6,5 +6,12 @@ public class Main {
     //then i gets incremented by 1 and repeat the processes till i is greater or = to 5 and stops before printing
         System.out.println("Hello World");
        }
+
+       for(int j = 5; j > 0; j--){
+        //initalizin j to 0; 
+        //check if j is greater than 0, moves to the next line and gets execute; 
+        //then j gets decremented by 1 and repeat the processes till j is less than 0 and stops 
+            System.out.println("Hello World " + j);
+           }
    } 
 }
