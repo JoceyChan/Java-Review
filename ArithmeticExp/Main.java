4911309d48030a07dbd66d0b0684aee2e4cc9e87
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-09 22:50:23 -0700

    Augmented or Compund operators

---
+++ ArithmeticExp/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args){
+        int result = 10/3; 
+        System.out.println(result);
+
+        double result2 = (double)10 / (double)3;
+        System.out.println(result2);
+
+        int x = 1;
+        int y = ++x; //x++ is one and ++x is 2
+        System.out.println(x);
+        System.out.println(y);
+
+        int a = 1;
+        a = a + 2;
+        a += 2;
+        System.out.println(a);
+
+        int b = 1;
+        b /= 2; // Augmented or Compound operators
+        System.out.println(b);
+
+    }
+    
+}
