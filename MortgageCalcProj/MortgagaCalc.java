558a90f97273cc1b1a37036ce6f6c0b963ee22a2
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 02:41:43 -0700

    changed into int

--- MortgageCalcProj/MortgagaCalc.java
+++ MortgageCalcProj/MortgagaCalc.java
@@ -1,23 +1,14 @@
+import java.time.YearMonth;
 import java.util.Scanner;
 public class MortgagaCalc {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         System.out.print("Principle: ");
-        double p = scan.nextDouble();
+        int p = scan.nextInt();
         System.out.print("Annual Interest Rate: ");
         double r = scan.nextDouble();
         System.out.print("Number of payments: ");
         double n = scan.nextDouble();
 
-        double a = 1 + r;
-        double top = r * Math.pow(a, n);
-        System.out.println(a);
-        System.out.println(top);
-
-        double bottom = Math.pow(a, n) - 1;
-        System.out.println(bottom);
-
-        double together = p * (top/bottom);
-        System.out.println("Mortgage Payment: " + together);
     }
 }
