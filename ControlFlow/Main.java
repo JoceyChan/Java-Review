da8cb36fab57ad20d54a41ae2e1e26be73edb066
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 10:43:48 -0700

    operators

---
+++ ControlFlow/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+        int x = 1;
+        int y = 1;
+
+        System.out.println(x == y); // equals
+        System.out.println(x != y); // "!=" does not equal
+        System.out.println(x < y); // x is less than y
+        System.out.println(x <= y); // x is less than or equal to y
+
+
+    }
+}
