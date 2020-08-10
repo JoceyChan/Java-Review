f7e3598095c9700d638677b6fbebaa3768c2ff94
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 12:20:42 -0700

    divisble by 3 dividble by 3 and 5

--- Excercise/FizzBuzz.java
+++ Excercise/FizzBuzz.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 public class FizzBuzz {
     public static void main(String[] args) {
-        //number if number is divisbale by 5 print fizz
-        // if 10 print fizz 
     //divisible by 3 pribt buzz
     // if divisible by both 5 and 3 print fizz buzz
     // if not divisble the print number
@@ -12,9 +10,13 @@ public class FizzBuzz {
 
     int n = scan.nextInt();
 
-    if (n % 5 == 0){
+    if (n % 5 == 0)
         System.out.println("Fizz");
-    }
+    else if (n % 3 == 0)
+        System.out.println("Buzz");
+    else if ((n % 5 == 0) && (n % 3 == 0))
+        System.out.println("Fizz Buzz");
+
         
     }
 }
