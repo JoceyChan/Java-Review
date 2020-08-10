82238ba707b48de1dfd483e40c6bd8e7bf583e0c
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-09 23:41:04 -0700

    notes

--- Casting/Cast.java
+++ Casting/Cast.java
@@ -1,10 +1,15 @@
 public class Cast {
     public static void main(String[] args){
         //implicit casting, automatic casting or automatic conversion
-        // byte > short > int > long
+        // byte > short > int > long > float > double
+        // When casting there is no data loss when casting 
         short x = 1; // short has two bytes 
         int y = x + 2; //int has 4 bytes
         System.out.println(y);
+
+        double a = 1.1;
+        double b = a + 2;
+        System.out.println(a);
     }
     
 }
