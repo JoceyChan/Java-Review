75a257f225e6ecc071176fe08af9b16abfcd396f
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 02:04:14 -0700

    Created print statments, declared/initalized vars

--- MortgageCalcProj/MortgagaCalc.java
+++ MortgageCalcProj/MortgagaCalc.java
@@ -2,5 +2,14 @@ import java.util.Scanner;
 public class MortgagaCalc {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
+        System.out.print("Principle: ");
+        int p = scan.nextInt();
+        System.out.print("Monthly interest rate: ");
+        int r = scan.nextInt();
+        System.out.print("Number of payments: ");
+        int n = scan.nextInt();
+        System.out.print("Mortgage: ");
+        int m = scan.nextInt();
+
     }
 }
