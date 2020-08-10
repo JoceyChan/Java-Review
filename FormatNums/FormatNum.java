cd9fb621daccaf10d0dd959006426c636da1a7af
Jocey Chan <joceychan@franciscos-mbp.home>
2020-08-10 01:22:13 -0700

    Percentage and Currency Format

--- FormatNums/FormatNum.java
+++ FormatNums/FormatNum.java
@@ -1,7 +1,20 @@
+import java.text.NumberFormat;
+
 public class FormatNum {
     public static void main(String[] args){
         // if you want to display this to the user add a "$" and every three digits add a commas as the following:
         // $1,234,567 
+       // NumberFormat currency = new NumberFormat(); // we can not create and instance of a class as it is an abstract
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String result = currency.format(1234567.891);
+        System.out.println(result);
+
+        NumberFormat percentage = NumberFormat.getPercentInstance(); //Shift + F6 = changes the subject and the refrence at the same time
+        String result2 = percentage.format(1234567.891);
+        System.out.println(result2);
+
+        String results = NumberFormat.getPercentInstance().format(0.1); //Same thing as the top but using .format instead and not using a variable 
+        System.out.println(results);
     }
     
 }
