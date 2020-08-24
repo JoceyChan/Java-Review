86c8ffaa4450f8b8486a652f9fc92a868446a01d
Jocey Chan <joceychan@Franciscos-MBP.attlocal.net>
2020-08-24 15:41:00 -0700

    class

--- SinglyLinkedList/Employee.java
+++ SinglyLinkedList/Employee.java
@@ -26,4 +26,32 @@ public class Employee {
     public void setId(int id){
         this.id = id;
     }
+    public boolean equals(Object o){
+        if(this == o){
+            return true;
+        }
+        if(o == null || getClass() != o.getClass()){
+            return false;
+        }
+        Employee employee = (Employee) o;
+        if(id != employee.id){
+            return false;
+        }
+        if(!firstName.equals(employee.firstName)){
+            return false;
+        }
+        return lastName.equals(employee.lastName);
+    }
+    public int hashCode(){
+        int result = firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + id;
+        return result;
+    }
+    public String toString(){
+        return "Employee{" + 
+                "firstName= '" + firstName + '\'' +
+                ", lastName= '" + lastName + '\'' +
+                ", id = " + id + '}';
+    }
 }
