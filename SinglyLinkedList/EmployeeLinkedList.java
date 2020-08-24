563286c7727fe68a8657277cd58d738e58cd95f6
Jocey Chan <joceychan@Franciscos-MBP.attlocal.net>
2020-08-24 15:53:16 -0700

    created addToFront method

---
+++ SinglyLinkedList/EmployeeLinkedList.java
@@ -0,0 +1,11 @@
+package SinglyLinkedList;
+
+public class EmployeeLinkedList {
+    private EmployeeNode head;
+
+    public void addToFront(Employee employee){
+        EmployeeNode node = new EmployeeNode(employee);
+        node.setNext(head);
+        head = node; 
+    }
+}
